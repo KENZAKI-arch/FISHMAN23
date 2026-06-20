3d64a3a5d4dc9f6aa61fa8d0eae88f300ff38f96
KENZAKI-arch <luisgabrielcalilap@gmail.com>
2026-06-20 14:10:50 +0000

    Jun 20, 2026, 10:10 PM

--- autofish.lua
+++ autofish.lua
@@ -935,7 +935,7 @@ addConn(RunService.Heartbeat:Connect(function(dt)
 end))
 
 -- ======================================================================
--- 👻 TRUE NOCLIP (Tied to Physics Engine)
+-- 👻 TRUE NOCLIP (Tied to Physics Engine)1
 -- ======================================================================
 addConn(RunService.Stepped:Connect(function()
     if not _running then return end
