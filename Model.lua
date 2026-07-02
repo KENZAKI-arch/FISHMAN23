345a09c59c1642d1f45ae19d041661022d527939
KENZAKI-arch <luisgabrielcalilap@gmail.com>
2026-07-02 22:15:15 +0800

    Revert swingsfx and task.spawn optimizations in combat loop due to game anti-cheat requirements

--- Model.lua
+++ Model.lua
@@ -205,6 +205,22 @@ function Model.DoCombatCombo()
         if not character or not character:FindFirstChild("HumanoidRootPart") then break end
         
         local myCFrame = character.HumanoidRootPart.CFrame
+        local animName = "Punch" .. currentHit
+        local punchAnim = ReplicatedStorage:WaitForChild("CombatAnimations", 9e9):WaitForChild("Melee", 9e9):WaitForChild(animName, 9e9)
+        
+        local swingArgs = {
+            [1] = {
+                [1] = "swingsfx",
+                [2] = "Melee",
+                [3] = currentHit,
+                [4] = "Ground",
+                [5] = false,
+                [6] = punchAnim,
+                [7] = 2,
+                [8] = 1.5
+            }
+        }
+        task.spawn(function() pcall(function() combatRegister:InvokeServer(unpack(swingArgs)) end) end)
         
         task.wait(0.35) 
         
@@ -228,7 +244,7 @@ function Model.DoCombatCombo()
                     ["aircombo"] = "Ground"
                 }
             }
-            pcall(function() combatRegister:InvokeServer(unpack(damageArgs)) end)
+            task.spawn(function() pcall(function() combatRegister:InvokeServer(unpack(damageArgs)) end) end)
         end
         task.wait(0.2)
     end
