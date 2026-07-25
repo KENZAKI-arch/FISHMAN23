fe084e6cbdfbd2bd5943ab53f267a1d0e245c9aa
KENZAKI-arch <luisgabrielcalilap@gmail.com>
2026-07-25 14:34:41 +0800

    Hardcode Second Sea ConfirmationPrompt remote line

--- MSTACK/joinersystem.lua
+++ MSTACK/joinersystem.lua
@@ -2034,11 +2034,11 @@ Tabs = {
                         task.wait(0.5)
 
                         -- Step 2: Confirm on ConfirmationPrompt
-                        local args2 = {
+                        local args = {
                             [1] = "Second Sea";
                         }
-                        local confirmationPrompt = LocalPlayer:WaitForChild("PlayerGui", 9e9):WaitForChild("ConfirmationPrompt", 9e9)
-                        confirmationPrompt:WaitForChild("RemoteEvent", 9e9):FireServer(unpack(args2))
+
+                        game:GetService("Players"):WaitForChild("JamesLeeHNH", 9e9):WaitForChild("PlayerGui", 9e9):WaitForChild("ConfirmationPrompt", 9e9):WaitForChild("RemoteEvent", 9e9):FireServer(unpack(args))
                     else
                         remoteEvent:FireServer(unpack(confirmArgs))
                     end
