583c10ead1277b0a775139c57b1678cb4687d521
KENZAKI-arch <luisgabrielcalilap@gmail.com>
2026-08-18 16:13:39 +0800

    Added third intermediate waypoint right before Lever Room

--- MSTACK/Project Impel/Stage2.lua
+++ MSTACK/Project Impel/Stage2.lua
@@ -13,6 +13,7 @@ getgenv().MACRO_WAYPOINTS = {
     { Pos = Vector3.new(3465, 2378, -20344), Action = "NAVIGATE" }, -- F2 Corridor
     { Pos = Vector3.new(3449, 2378, -20377), Action = "NAVIGATE" }, -- F2 Corridor Midpoint
     { Pos = Vector3.new(3462, 2378, -20618), Action = "NAVIGATE" }, -- F2 Corridor p2
+    { Pos = Vector3.new(3383, 2343, -20565), Action = "NAVIGATE" }, -- Pre-Lever Room
     { Pos = Vector3.new(3198, 2343, -20539), Action = "PULL_LEVER" }, -- Lever Room
     { Pos = Vector3.new(3204, 2378, -20402), Action = "NAVIGATE" }, -- Reverse Path (Walk out of lever room)
     { Pos = Vector3.new(3200, 2375, -20737), Action = "END_MAZE" }  -- Final Boss Room
