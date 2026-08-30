d919fa6d4a944bb5ff03cca28a42a28aee24572b
KENZAKI-arch <luisgabrielcalilap@gmail.com>
2026-08-30 13:09:36 +0800

    Increase Cyborg Autofarm hoverboard firing range from 250 to 2500 to fix Meg Stack delay

--- MSTACK/protov4_nofactory.lua
+++ MSTACK/protov4_nofactory.lua
@@ -568,8 +568,7 @@ View.Build(function(isFarming)
                         
                         local targetSpot = rootPart.Position
                         local isAtHoverSpot = false
-                        
-                        if targetRoot then
+                                        if targetRoot then
                             if currentEnemy.Name == "Megalodon" and getgenv().CachedHoverboard and getgenv().CachedHoverboard.Parent then
                                 -- If we're anchored to the hoverboard, just check if we're in range!
                                 local dist = (rootPart.Position - targetRoot.Position).Magnitude
