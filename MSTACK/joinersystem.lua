72506280339d6ea73d3ea988d4cc321ae9beab15
KENZAKI-arch <luisgabrielcalilap@gmail.com>
2026-08-30 12:54:56 +0800

    Update UI topbar title to v4.1

--- MSTACK/joinersystem.lua
+++ MSTACK/joinersystem.lua
@@ -2432,7 +2432,7 @@ end
 
 local Window = Fluent:CreateWindow({
     Title = "🐟 Fishman Hub",
-    SubTitle = "Unified Auto-Fisher 1.0.3 v4.05",
+    SubTitle = "Unified Auto-Fisher 1.0.3 v4.1",
     TabWidth = 160,
     MinimizeKey = Enum.KeyCode.RightShift
 })
