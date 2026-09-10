f7bbadfe3cd206e884c4f2e9baedb5757750b4da
KENZAKI-arch <luisgabrielcalilap@gmail.com>
2026-09-10 15:23:05 +0800

    chore(ui): update topbar version to 3.9

--- MSTACK/Modularized/Config.lua
+++ MSTACK/Modularized/Config.lua
@@ -1,6 +1,6 @@
 -- Config & State Setup
 getgenv().FishmanState = getgenv().FishmanState or {}
--- Version 3.8
+-- Version 3.9
 -- ======================================================================
 -- 🛑 GLOBAL SETUP & DUPLICATE PREVENTION
 -- ======================================================================
